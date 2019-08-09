36453fdebd4fb2aadd6dfcec6fcbe2f49d99465e
tacticaltofu <dingkevin@gmail.com>
2019-08-09 12:12:03 -0400

    about

--- aesthetic/models.py
+++ aesthetic/models.py
@@ -93,16 +93,16 @@ def populate_db():
 
     admin_1 = Post(title='rezero', content='<img src="https://i.imgur.com/Y1eFs13.png" width="560px">', author=admin)
     admin_2 = Post(title='hannibal for king', content='<iframe width="560" height="315" src="https://www.youtube.com/embed/A2F51rFYSfo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>', author=admin, date_posted=datetime.today()-timedelta(days=1))
-    admin_3 = Post(title='How to get 15 inch arms from bodyweight only', content='3 sets of 8m 20kg weighted chin ups\n 3 sets of 8 dips', author=admin, date_posted=datetime.today()-timedelta(days=2))
+    admin_3 = Post(title='How to get 15 inch arms from bodyweight only', content='3 sets of 8 20kg weighted chin ups\n 3 sets of 8 dips', author=admin, date_posted=datetime.today()-timedelta(days=2))
     admin_4 = Post(title='have you seen alien?', content='no i habe not seen alien', author=admin, date_posted=datetime.today()-timedelta(days=3))
-    admin_5 = Post(title='stop snitching', content='bitch nigga snitch nigga hoe nigga bitch nigga bitch nigga snitch nigga hoe nigga bitch nigga me and bitch niggas we dont conversate!', author=admin, date_posted=datetime.today()-timedelta(days=5))
+    admin_5 = Post(title='stop snitching', content='we dont conversate!', author=admin, date_posted=datetime.today()-timedelta(days=5))
 
     bob_1 = Post(title='Triceps vs Biceps vs Forearms: Which contributes the most to arm size?', content=filler, author=bob)
     bob_2 = Post(title='how to gain mass', content=filler, author=bob, date_posted=datetime.today()-timedelta(days=3))
     bob_3 = Post(title='how to heal from bulging disc', content=filler, author=bob, date_posted=datetime.today()-timedelta(days=5))
     bob_4 = Post(title='shoulder rehab protocol', content=filler, author=bob, date_posted=datetime.today()-timedelta(days=6))
 
-    test_1 = Post(title='riley reid vs sasha grey', content=filler, author=test, date_posted=datetime.today()-timedelta(days=1))
+    test_1 = Post(title='never gonna give u up vs africa by toto', content=filler, author=test, date_posted=datetime.today()-timedelta(days=1))
     test_2 = Post(title='potato salad', content=filler, author=test, date_posted=datetime.today()-timedelta(days=2))
     test_3 = Post(title='reps for jesus', content=filler, author=test, date_posted=datetime.today()-timedelta(days=3))
     
