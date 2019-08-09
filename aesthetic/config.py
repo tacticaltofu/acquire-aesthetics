6659e10ee13dc2ae0d903a5f60febdb29d07a68e
tacticaltofu <dingkevin@gmail.com>
2019-08-09 15:32:03 -0400

    heroku deployment

--- aesthetic/config.py
+++ aesthetic/config.py
@@ -2,4 +2,5 @@ import os
 
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
