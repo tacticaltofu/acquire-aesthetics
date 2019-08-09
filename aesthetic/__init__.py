99f111b9fe4f3105edf317f4c05120022241d3d6
tacticaltofu <dingkevin@gmail.com>
2019-08-09 18:22:52 -0400

    Revert "heroku deployment"
    This reverts commit 6659e10ee13dc2ae0d903a5f60febdb29d07a68e.

--- aesthetic/__init__.py
+++ aesthetic/__init__.py
@@ -30,22 +30,4 @@ def create_app(config_class=Config):
 	app.register_blueprint(main)
 	app.register_blueprint(errors)
 
-	if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/aesthetic.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Aesthetic startup')
-
 	return app
--- aesthetic/config.py
+++ aesthetic/config.py
@@ -2,5 +2,4 @@ import os
 
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
