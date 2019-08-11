9451d6f0ed1b0ab99767927e031202a26f18f079
tacticaltofu <dingkevin@gmail.com>
2019-08-10 20:48:35 -0400

    deployment changes

--- aesthetic/__init__.py
+++ aesthetic/__init__.py
@@ -1,11 +1,16 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flaskext.markdown import Markdown
 from aesthetic.config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
@@ -17,6 +22,7 @@ def create_app(config_class=Config):
 	app.jinja_env.globals.update(count=len)
 
 	db.init_app(app)
+	migrate.init_app(app, db)
 	bcrypt.init_app(app)
 	login_manager.init_app(app)
 	Markdown(app)
@@ -29,5 +35,23 @@ def create_app(config_class=Config):
 	app.register_blueprint(posts)
 	app.register_blueprint(main)
 	app.register_blueprint(errors)
+
+	if not app.debug and not app.testing:
+		if app.config['LOG_TO_STDOUT']:
+			stream_handler = logging.StreamHandler()
+			stream_handler.setLevel(logging.INFO)
+			app.logger.addHandler(stream_handler)
+		else:
+			if not os.path.exists('logs'):
+				os.mkdir('logs')
+			file_handler = RotatingFileHandler('logs/aesthetic.log', maxBytes=10240, backupCount=10)
+			file_handler.setFormatter(logging.Formatter(
+				'%(asctime)s %(levelname)s: %(message)s '
+				'[in %(pathname)s:%(lineno)d]'))
+			file_handler.setLevel(logging.INFO)
+			app.logger.addHandler(file_handler)
+
+		app.logger.setLevel(logging.INFO)
+		app.logger.info('Aesthetic startup')
 
 	return app
