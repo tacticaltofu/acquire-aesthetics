a70f459ab2e3dee737dab955e4cb70bb206e7e3e
tacticaltofu <dingkevin@gmail.com>
2019-12-12 19:32:15 -0500

    add everything

--- aesthetic/config.py
+++ aesthetic/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+	SECRET_KEY = os.environ.get('SECRET_KEY') or '5c404840f211c0609a21a1eaeb5dda44'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT') or 1
--- aesthetic/models.py
+++ aesthetic/models.py
@@ -36,7 +36,7 @@ class Comment(db.Model):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=True)
-    
+
     def __repr__(self):
         return f"Post('{self.date_posted}', '{self.content}')"
 
@@ -80,57 +80,3 @@ def init_db():
 
 def ensure_db_exists():
     db.create_all()
-
-def populate_db():
-    filler = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
-    
-    admin = User(username='admin', password=bcrypt.generate_password_hash('admin').decode('utf-8'))
-    bob = User(username='bob', password=bcrypt.generate_password_hash('123').decode('utf-8'))
-    test = User(username='tester', password=bcrypt.generate_password_hash('password').decode('utf-8'))
-
-    admin_1 = Post(title='rezero', content='<img src="https://i.imgur.com/Y1eFs13.png" width="560px">', author=admin)
-    admin_2 = Post(title='hannibal for king', content='<iframe width="560" height="315" src="https://www.youtube.com/embed/A2F51rFYSfo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>', author=admin, date_posted=datetime.today()-timedelta(days=1))
-    admin_3 = Post(title='How to get 15 inch arms from bodyweight only', content='3 sets of 8 20kg weighted chin ups\n 3 sets of 8 dips', author=admin, date_posted=datetime.today()-timedelta(days=2))
-    admin_4 = Post(title='have you seen alien?', content='no i habe not seen alien', author=admin, date_posted=datetime.today()-timedelta(days=3))
-    admin_5 = Post(title='stop snitching', content='we dont conversate!', author=admin, date_posted=datetime.today()-timedelta(days=5))
-
-    bob_1 = Post(title='Triceps vs Biceps vs Forearms: Which contributes the most to arm size?', content=filler, author=bob)
-    bob_2 = Post(title='how to gain mass', content=filler, author=bob, date_posted=datetime.today()-timedelta(days=3))
-    bob_3 = Post(title='how to heal from bulging disc', content=filler, author=bob, date_posted=datetime.today()-timedelta(days=5))
-    bob_4 = Post(title='shoulder rehab protocol', content=filler, author=bob, date_posted=datetime.today()-timedelta(days=6))
-
-    test_1 = Post(title='never gonna give u up vs africa by toto', content=filler, author=test, date_posted=datetime.today()-timedelta(days=1))
-    test_2 = Post(title='potato salad', content=filler, author=test, date_posted=datetime.today()-timedelta(days=2))
-    test_3 = Post(title='reps for jesus', content=filler, author=test, date_posted=datetime.today()-timedelta(days=3))
-    
-
-    db.session.add(admin)
-    db.session.add(admin_1)
-    db.session.add(admin_2)
-    db.session.add(admin_3)
-    db.session.add(admin_4)
-    db.session.add(admin_5)
-
-    db.session.add(bob)
-    db.session.add(bob_1)
-    db.session.add(bob_2)
-    db.session.add(bob_3)
-    db.session.add(bob_4)
-
-    db.session.add(test)
-    db.session.add(test_1)
-    db.session.add(test_2)
-    db.session.add(test_3)
-
-    db.session.commit()
-
-
-def ghetto_migrate():
-    from aesthetic import create_app
-    app = create_app()
-    with app.app_context():
-        init_db()
-        populate_db()
